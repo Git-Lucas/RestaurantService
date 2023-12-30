cbf113e0272262de9a175af7efb2dc5803114947
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2023-12-30 15:13:16 -0300

    feat: Adicionados os campos restantes no objeto 'Restaurant' do projeto 'ItemService', para o mapeamento do objeto proveniente do projeto 'RestaurantService' seja completo

--- ItemService/Models/Restaurant.cs
+++ ItemService/Models/Restaurant.cs
@@ -8,5 +8,7 @@ public class Restaurant
     public required int Id { get; set; }
     public required int ExternalId { get; set; }
     public required string Name { get; set; }
+    public required string Address { get; set; }
+    public required string Site { get; set; }
     public ICollection<Item> Items { get; set; } = new List<Item>();
 }
